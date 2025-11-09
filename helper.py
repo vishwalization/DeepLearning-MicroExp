fac80826eaafaf153fc5c8493211af0d2e9bc486
vishwa-eve <vishwanathan@meetwitheve.com>
2025-11-09 14:18:43 +0530

    feat: add Linear Algebra

---
+++ helper.py
@@ -0,0 +1,5 @@
+def sz(data_structure):
+    """
+    Takes a numpy data structure and prints its shape and dimension 
+    """
+    print(f'shape: {data_structure.shape} dim: {data_structure.ndim}')
